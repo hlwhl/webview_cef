f8441552276c6e69437ef114bcd68ce24f0286c8
Prome <levi.hao96@gmail.com>
2022-09-12 18:31:24 +0800

    [Windows] windows & macos should share same cpp code base

--- common/simple_app.h
+++ common/simple_app.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
+﻿// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
@@ -23,8 +23,10 @@ public:
                                        CefRefPtr<CefCommandLine> command_line) override {
                                            command_line->AppendSwitch("disable-gpu");
                                            command_line->AppendSwitch("disable-gpu-compositing");
-                                           command_line->AppendSwitch("use-mock-keychain");
-                                           command_line->AppendSwitch("single-process");
+                                           #ifdef __APPLE__
+                                                command_line->AppendSwitch("use-mock-keychain");
+                                                command_line->AppendSwitch("single-process");
+                                           #endif
                                        }
     
     // CefBrowserProcessHandler methods:
